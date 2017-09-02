6d7ac6f52e7964139c35b3dc041b495a813152e7
serax75 <kevin.lane.foster@gmail.com>
2017-09-02 22:37:58 +0000

    testing search text

--- bot.js
+++ bot.js
@@ -9,22 +9,20 @@ var cards = '';
 var cardID = '';
 var jsonObj = '';
 
-function getCards () {
-  request.get({
-    url: url,
-    json: true,
-    headers: {'User-Agent': 'request'}
-  }, (err, res, data) => {
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-      // data is already parsed as JSON:
-      console.log(data.html_url);
-    }
+request.get({
+  url: url,
+  json: true,
+  headers: {'User-Agent': 'request'}
+}, (err, res, data) => {
+  if (err) {
+     console.log('Error:', err);
+  } else if (res.statusCode !== 200) {
+    console.log('Status:', res.statusCode);
+  } else {
+    // data is already parsed as JSON:
+    console.log(data.html_url);
+  }
 });
-}
  
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
