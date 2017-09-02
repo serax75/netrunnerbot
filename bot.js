446150e3786197eb467faf7a4da7e081ce9933a5
serax75 <kevin.lane.foster@gmail.com>
2017-09-02 18:36:51 +0000

    testing search text

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^!card (.*)$/;
+      searchText = $1;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -21,7 +22,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "testing testing";
+  botResponse = "searchText\n";
 
   options = {
     hostname: 'api.groupme.com',
